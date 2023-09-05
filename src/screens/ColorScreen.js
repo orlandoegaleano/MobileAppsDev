4035ccb5e550aab64e15ae93c8919deb684417ab
orlandoegaleano <orlandoegaleano@gmail.com>
2023-09-05 12:55:34 -0500

    Continued from 9/5 lecture
    Finished the add color screen. Learned about Dimensions and implemented a way to display the color boxes in a way that maximizes the number of boxes displayed in a row depending on the screen's size

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,28 +1,60 @@
 import React, { useState } from "react";
-import {Text, Button, View, StyleSheet, } from "react-native";
+import {Text, Button, View, StyleSheet, FlatList, Dimensions } from "react-native";
+
+
+ //Using Dimensions to get the screen dimesions of the device being used in order to determine how many columns can be displayed.
+ const screenWidth = Dimensions.get('window').width;
+ const itemWidth = 100; // or whatever your item width is
+ const itemMargin = 10; // margin for your items
+ const itemsPerRow = Math.floor(screenWidth / (itemWidth + itemMargin * 2));
 
 const ColorScreen = () => {
+
    
-    //const [counter, setCounter] = useState(0);
+   
+    const [colors, setColors] = useState([]);
+    console.log(colors);
 
     return (
-    <View>
+    <View backgroundColor='rgb(0,0,0)'>
 
         <Button title="Add color" onPress={()=>{
-            
+            setColors([...colors, randomRgb()])
+        }}/>
 
-        }}/> 
-
-        <View>
-
-        </View>    
+        <View>     
+            <FlatList                
+                data={colors}
+                keyExtractor={(item) => item}
+                renderItem={({item}) => (
+                    <View style={[styles.colorBox, {backgroundColor: item}]}>
+                    </View>                  
+                )}
+                numColumns={itemsPerRow} 
+            />
+        </View>
 
     </View>  
     )
 }
 
-const styles = StyleSheet.create({
+const randomRgb = () => {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
 
+    let colorString = `rgb(${red}, ${green}, ${blue})`;
+
+    return colorString;
+}
+
+const styles = StyleSheet.create({
+    colorBox: {
+        width: itemWidth,
+        height: 100,
+        margin: itemMargin,      
+
+    },
 });
 
 export default ColorScreen
